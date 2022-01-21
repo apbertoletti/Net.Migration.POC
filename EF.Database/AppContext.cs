63d0160d735c81484ed31ffd1c1f80a09e940ce8
André Bertoletti <abertoletti@luxfacta.com>
2022-01-21 11:01:24 -0300

    Adding custom columns in EFMigrationHistory table

--- EF.Database/AppContext.cs
+++ EF.Database/AppContext.cs
@@ -1,6 +1,11 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Microsoft.EntityFrameworkCore.SqlServer.Migrations.Internal;
+using Microsoft.EntityFrameworkCore.Storage;
 using Microsoft.Extensions.Logging;
 using System;
+using System.Text;
 
 namespace EF.Database
 {
@@ -19,7 +24,61 @@ namespace EF.Database
             optionsBuilder
                 .UseSqlServer(_connectionString)
                 .LogTo(Console.WriteLine, LogLevel.Information)
+                .ReplaceService<IHistoryRepository, MyHistoryRepository>()
                 .EnableSensitiveDataLogging();
+        }
+    }
+
+    internal class MyHistoryRepository : SqlServerHistoryRepository
+    {
+        public MyHistoryRepository(HistoryRepositoryDependencies dependencies)
+            : base(dependencies)
+        {
+        }
+
+        protected override void ConfigureTable(EntityTypeBuilder<HistoryRow> history)
+        {
+            base.ConfigureTable(history);
+
+            history.Property(h => h.MigrationId)
+                .HasColumnName("Name")
+                .HasMaxLength(200);
+
+            history.Property(h => h.ProductVersion)
+                .HasColumnName("VersionEF");
+
+            history.Property<DateTime>("Applied")
+                .IsRequired(true);
+
+            history.Property<string>("User")
+                .IsRequired(true)
+                .HasMaxLength(200);
+
+            history.Property<string>("Machine")
+                .IsRequired(true)
+                .HasMaxLength(200);
+        }
+
+        public override string GetInsertScript(HistoryRow row)
+        {
+            var stringTypeMapping = Dependencies.TypeMappingSource.GetMapping(typeof(string));
+            return new StringBuilder().Append("INSERT INTO ")
+                .Append(SqlGenerationHelper.DelimitIdentifier(TableName, TableSchema))
+                .Append(" (")
+                .Append(SqlGenerationHelper.DelimitIdentifier(MigrationIdColumnName))
+                .Append(", ")
+                .Append(SqlGenerationHelper.DelimitIdentifier(ProductVersionColumnName))
+                .Append(", [Applied], [User], [Machine])")
+                .Append("VALUES (")
+                .Append(stringTypeMapping.GenerateSqlLiteral(row.MigrationId))
+                .Append(", ")
+                .Append(stringTypeMapping.GenerateSqlLiteral(row.ProductVersion))
+                .Append($", GETDATE()")
+                .Append($", '{Environment.UserName}'")
+                .Append($", '{Environment.MachineName}'")
+                .Append(")")
+                .AppendLine(SqlGenerationHelper.StatementTerminator)
+                .ToString();
         }
     }
 }
