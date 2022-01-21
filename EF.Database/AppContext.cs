32457f113818c4f07300b064e0e1d7905a992a27
André Bertoletti <abertoletti@luxfacta.com>
2022-01-21 09:19:23 -0300

    Asking the target database feature

--- EF.Database/AppContext.cs
+++ EF.Database/AppContext.cs
@@ -1,22 +1,23 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace EF.Database
 {
     public class AppContext : DbContext
     {
         //The connection string can be read from the some external file
-        const string connectionString = "Data Source=(LocalDb)\\MSSQLLocalDB;Initial Catalog=NomeBDPreExistente;Integrated Security=true";
+        private string _connectionString;
+
+        public AppContext(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder
-                .UseSqlServer(connectionString)
+                .UseSqlServer(_connectionString)
                 .LogTo(Console.WriteLine, LogLevel.Information)
                 .EnableSensitiveDataLogging();
         }
--- EF.Database/Program.cs
+++ EF.Database/Program.cs
@@ -5,9 +5,45 @@ namespace EF.Database
 {
     class Program
     {
+        private static string connectionString_Sandbox = "Data Source=(LocalDb)\\MSSQLLocalDB;Initial Catalog=NomeBDPreExistente;Integrated Security=true";
+
+        private static string connectionString_Production = "Data Source=(LocalDb)\\MSSQLLocalDB;Initial Catalog=NomeBDPreExistente_prod;Integrated Security=true";
+
         static int Main(string[] args)
         {
-            var context = new AppContext();
+            Console.WriteLine("Do you want to run the migrations in the [S]andbox or [P]roduction target database?");
+            var targetDatabase = Console.ReadLine();            
+
+            AppContext context;
+            switch (targetDatabase.ToUpper())
+            {
+                case "S":
+                    context = new AppContext(connectionString_Sandbox);
+                    break;
+
+                case "P":
+                    Console.ForegroundColor = ConsoleColor.DarkRed;
+                    Console.BackgroundColor = ConsoleColor.DarkYellow;
+                    Console.WriteLine("Are you sure to run in PRODUCTION database? [Y]es or [N]o");
+                    Console.ResetColor();
+                    var responseProduction = Console.ReadLine();
+                    switch (responseProduction.ToUpper())
+                    {
+                        case "Y":
+                            context = new AppContext(connectionString_Production);
+                            break;
+
+                        default:
+                            Console.WriteLine("Migration aborted!");
+                            return -1;
+                    }
+                    break;
+
+                default:
+                    Console.WriteLine("Invalid target database!");
+                    Console.ReadKey();
+                    return -1;
+            }
 
             try
             {
